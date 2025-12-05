b693c75cb46cff0c609f7c2f446cfd6893ad8a6a
Azatov Murodbek <murodbekazatov126@gmail.com>
2025-12-05 14:59:55 +0500

    funksiya

---
+++ funksiya.py
@@ -0,0 +1,7 @@
+# def yosh_hisoblash(t_yil):
+#     natija = 2025 - t_yil
+#     if natija <= 5:
+#         return f"Siz juda yosh ekansiz, yoshingiz: {natija} yosh."
+#     return f"Sizning yoshingiz: {natija} yosh."
+# t_yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# print(yosh_hisoblash(t_yil))
